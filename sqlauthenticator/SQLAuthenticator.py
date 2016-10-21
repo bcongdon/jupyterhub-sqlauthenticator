386d2cf4807ced459128356056348af20bd69a93
Benjamin Congdon <bencongdon96@gmail.com>
2016-10-21 16:21:22 -0500

    Initial commit :sparkles:

---
+++ sqlauthenticator/SQLAuthenticator.py
@@ -0,0 +1,35 @@
+from jupyterhub.auth import Authenticator
+from tornado import gen
+from passlib.hash import pbkdf2_sha256
+import pymysql.cursors
+import os
+
+
+class SQLAuthenticator(Authenticator):
+    def _verify_password_hash(self, hash_, password):
+        return pbkdf2_sha256.verify(password, hash_)
+
+    @gen.coroutine
+    def authenticate(self, handler, data):
+        conn = pymysql.connect(host=os.getenv('MYSQL_HOST'),
+                               port=os.getenv('MYSQL_PORT'),
+                               user=os.getenv('MYSQL_USER'),
+                               password=os.getenv('MYSQL_PASS'),
+                               db=os.getenv('MYSQL_DB'),
+                               charset='utf8mb4',
+                               cursorclass=pymysql.cursors.DictCursor)
+
+        try:
+            with conn.cursor() as cursor:
+                user = conn.excape(data['username'])
+                sql = 'SELECT `password` FROM `users` WHERE `username` = {}'
+                sql_formatted = sql.format(user)
+
+                cursor.execute(sql_formatted)
+                result = cursor.fetchone()
+                if result and self._verify_password_hash(result['password'],
+                                                         data['password']):
+                    return data['username']
+
+        finally:
+            conn.close()
