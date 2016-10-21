f46a26c9dea2c6ae23c2282c557cb407c462d3ff
Benjamin Congdon <bencongdon96@gmail.com>
2016-10-21 16:27:54 -0500

    setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='sqlauthenticator',
+      version='1.0',
+      description='SQL Authenticator for Jupyterhub',
+      author='Benjamin Congdon',
+      author_email='bcongdo2@illinois.edu',
+      url='https://github.com/bcongdon/adsa-jupyterhub-authenticator',
+      packages=['sqlauthenticator'],
+     )
