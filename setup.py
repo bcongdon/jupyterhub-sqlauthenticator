6ce2a2304bb254f086f2eab714f9900604bbccaa
Benjamin Congdon <bencongdon96@gmail.com>
2016-10-21 19:44:12 -0500

    Add dependencies to setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,13 @@ setup(name='sqlauthenticator',
       version='1.0',
       description='SQL Authenticator for Jupyterhub',
       author='Benjamin Congdon',
+      license='MIT',
       author_email='bcongdo2@illinois.edu',
       url='https://github.com/bcongdon/adsa-jupyterhub-authenticator',
       packages=['sqlauthenticator'],
-     )
+      install_requires=[
+        'passlib',
+        'PyMySQL',
+        'jupyterhub',
+      ],
+      )
